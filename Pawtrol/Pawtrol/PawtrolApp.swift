a833a645b4bfdb27360ac720c5de3a3f875a6e3b
Eduardo Richie <eduardorichie.work@gmail.com>
2026-08-11 11:31:52 +0800

    Initial Xcode project

---
+++ Pawtrol/Pawtrol/PawtrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawtrolApp.swift
+//  Pawtrol
+//
+//  Created by Eduardo Richie Imanuell on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PawtrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
